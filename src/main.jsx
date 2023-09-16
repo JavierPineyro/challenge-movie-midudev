1b446ffb6614346cba462642b85848ec11aae95f
JavierPineyro <javidunam22@gmail.com>
2023-09-16 14:20:12 -0300

    Feat: add dnd-react dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { NextUIProvider } from '@nextui-org/react'
-import './index.css'
 import { AppProvider } from './context/books-context.jsx'
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend"
+
+import './index.css'
+
 
 // npm install use-dark-mode a dependecy to change the theme of the app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <NextUIProvider disableBaseline="true">
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </NextUIProvider>,
+  <DndProvider backend={HTML5Backend}>
+    <NextUIProvider disableBaseline="true">
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </NextUIProvider>
+  </DndProvider>,
 )
