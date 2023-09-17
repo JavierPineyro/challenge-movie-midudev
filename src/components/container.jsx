7b65d0cbfc0d11d3bee7874809f5fb0fcb73fd8b
JavierPineyro <javidunam22@gmail.com>
2023-09-17 19:58:20 -0300

    Styles: Change styling and theme

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,6 +1,6 @@
 function Container({ children }) {
   return (
-    <main className="grid grid-rows-[70px,1fr,60px] min-h-screen max-w-screen-lg m-auto">
+    <main className="grid grid-rows-[70px,1fr,60px] sm:px-4 min-h-screen max-w-screen-lg m-auto">
       {children}
     </main>
   )
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,5 +1,5 @@
 const Title = ({ children }) => (
-  <h1 className="text-2xl py-2 font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white">
+  <h1 className="text-2xl flex-grow py-2 font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white">
     {children} 📚
   </h1>
 )
